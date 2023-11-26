39abd022adc8e9afdee0f7e9f6df83b11550d1fd
Eribert Marquez <147210473+erigenesis@users.noreply.github.com>
2023-11-26 00:33:24 -0400

    seed module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppModules } from './modules';
+import { SeedModule } from './seed/seed.module';
+import { SeedRepositoryService } from './seed.repository/seed.repository.service';
 
 @Module({
-  imports: [...AppModules],
+  imports: [...AppModules, SeedModule],
+  providers: [SeedRepositoryService],
 })
 export class AppModule {}
